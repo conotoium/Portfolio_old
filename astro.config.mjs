7fca745cfef233caef7bee13ed3732dce352ef9f
Contonium365 <87113453+conotoium@users.noreply.github.com>
2024-07-15 01:09:34 +0930

    oi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://conotoium.github.io',
+  base: 'Portfolio',
+  security: {
+		checkOrigin: true
+	},
+  integrations: [vue(), react(), sitemap(), tailwind()]
+});
