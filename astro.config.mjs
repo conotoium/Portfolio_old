b1d6fd48688d5bcae88298a7ac38028981e5c9c0
Contonium365 <87113453+conotoium@users.noreply.github.com>
2024-07-25 13:44:21 +0930

    "add search function"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,19 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://conotoium.github.io',
+  site: 'http://localhost:4322/Portfolio',
   base: 'Portfolio',
   security: {
-		checkOrigin: true
-	},
-  integrations: [vue(), react(), sitemap(), tailwind()]
+    checkOrigin: true
+  },
+  integrations: [vue(), react(), sitemap(), tailwind(), robotsTxt()],
+  output: "server",
+  adapter: vercel()
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="astro/client" />
+AUTH_SECRET=c2c28123ed5f710b640802d7768f6511
